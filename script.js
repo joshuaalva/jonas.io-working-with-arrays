f1f728b06acd20c37dff435ce5d080ed0d1be875
Joshua Alvarado <joshalva3@gmail.com>
2024-03-04 23:54:27 -0600

    added interest to account

--- script.js
+++ script.js
@@ -406,8 +406,12 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${out}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
 };
 
 calcDisplaySummary(account1.movements);
-
-//
