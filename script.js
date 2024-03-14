e077e9786cb7af6c07cfb60f53136cc83a4087c9
Joshua Alvarado <joshalva3@gmail.com>
2024-03-13 23:44:39 -0500

    coding challenge rewrite the calcaveragehuman age function using arrow syntax and chaining methods

--- script.js
+++ script.js
@@ -419,3 +419,30 @@ const calcDisplaySummary = function (movements) {
 };
 
 calcDisplaySummary(account1.movements);
+
+// <--- Coding Challenge #3 --->
+// rewrite the 'calcaveragehumanage' function from the previous challenge, but this time as an arrow function, and using chaining
+// const calcAverageHumanAge = function (ages) {
+//   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//   const adults = humanAges.filter(age => age >= 18);
+
+//   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+//   return average;
+// };
+
+// const avg1 = calcAverageHumanAge(testData1);
+// const avg2 = calcAverageHumanAge(testData2);
+// console.log(avg1, avg2);
+
+const testData1 = [5, 2, 4, 1, 15, 8, 3];
+const testData2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg1 = calcAverageHumanAge(testData1);
+const avg2 = calcAverageHumanAge(testData2);
+console.log(avg1, avg2);
